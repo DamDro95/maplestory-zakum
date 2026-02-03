0e7c70c48468203e201c415ce8f32191fd49331d
DamDro95 <68967801+DamDro95@users.noreply.github.com>
2026-02-03 04:44:51 +0000

    Build: (e8e4a67) testing actions

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig( {
-  base: '/maplestory-zakum/', // or 'https://cdn.example.com/assets/'
-  optimizeDeps: { exclude: ["@babylonjs/havok"] }
-} );
-
