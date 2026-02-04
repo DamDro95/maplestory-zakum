75f202051bce480125d40177a436014d22ff2b22
Damian <damian.drozd@hotmail.com>
2026-02-04 00:06:26 -0500

    Update product base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig( {
-  base: '/maplestory-zakum/', // or 'https://cdn.example.com/assets/'
-  optimizeDeps: { exclude: ["@babylonjs/havok"] }
-} );
-
+export default defineConfig( ( { command, mode, isSsrBuild, isPreview }) => {
+  if( command === 'serve' ){
+    return {
+      base: '/maplestory-zakum/', // or 'https://cdn.example.com/assets/'
+      optimizeDeps: { exclude: ["@babylonjs/havok"] }
+    }
+  }else{
+    return{
+      base: '/babylonjs/maplestory-zakum/', // or 'https://cdn.example.com/assets/'
+      optimizeDeps: { exclude: ["@babylonjs/havok"] }
+    }
+  }
+});
